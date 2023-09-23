259ecff41222a29d94f77d38ab312114bc0cec0c
solidity12 <volibear1001@gmail.com>
2023-09-24 00:28:35 +0900

    feat: Add UniswapV2Router02 address

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -23,3 +23,5 @@ main()
     console.error(error);
     process.exit(1);
   });
+// Deploying Uniswap V2 Router with the account: 0xa40aa030A3ba4f42FDCd2B7bC33d5B03770290ea
+// UniswapV2Router02 deployed to: 0xf4ED43C7F4f6739C6cf07ba7A1e8fdfCcb2e6f46
